e02ee34ea9fc827a3e731faaa28ae60916f65720
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2025-06-10 07:03:59 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿//*****************************************************************************
+//** 3442. Maximum Difference Between Even and Odd Frequency I      leetcode **
+//*****************************************************************************
+
+int maxDifference(char* s) {
+    int freq[26] = {0};
+
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        freq[s[i] - 'a']++;
+    }
+
+    int maxDiff = -100;
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] % 2 == 1)
+        {
+            for (int j = 0; j < 26; j++)
+            {
+                if (freq[j] % 2 == 0 && freq[j] > 0)
+                {
+                    int diff = freq[i] - freq[j];
+//                    printf("%d\n",diff);
+                    if (diff > maxDiff)
+                    {
+                        maxDiff = diff;
+                    }
+                }
+            }
+        }
+    }
+
+    return maxDiff;
+}
